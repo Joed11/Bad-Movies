a80ab1a96869aa98bfd9636a6c9b91ed048bd256
Joe Dimartino <j.dimartino11@gmail.com>
2020-09-19 20:44:01 -0400

    Define FavoriteMovies model and export

--- db/sql/index.js
+++ db/sql/index.js
@@ -1,4 +1,47 @@
-const mysql = require('mysql');
-const mysqlConfig = require('../../config.js');
+const Sequelize = require('sequelize');
+const mysql = require('mysql2');
+const mysqlConfig = require('../../config/config.js');
 
-const connection = mysql.createConnection(mysqlConfig);
+const badmovieDB = new Sequelize(mysqlConfig.database, mysqlConfig.user, mysqlConfig.password, {
+  dialect: 'mysql'
+})
+
+badmovieDB.authenticate()
+  .then(() => {
+    console.log('Connected to badmovies database')
+  })
+  .catch((err) => {
+    console.log('Error connecting to baddmovies database', err)
+  });
+
+
+var FavoriteMovies = badmovieDB.define('favoriteMovieList', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    unique: true,
+    primaryKey: true
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  popularity: {
+    type: Sequelize.DECIMAL(10,2),
+    allowNull: false,
+  },
+  overview: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  release_date: {
+    type: Sequelize.STRING,
+  },
+  thumbnail: {
+    type: Sequelize.STRING,
+  }
+});
+
+FavoriteMovies.sync({ alter: true });
+
+module.exports.FavoriteMovies = FavoriteMovies;
