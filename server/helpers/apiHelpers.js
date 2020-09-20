03397d4e103d52a381b2a3d3259d34d01a28ea0b
Joe Dimartino <j.dimartino11@gmail.com>
2020-09-19 20:45:16 -0400

    Add 2 methods to access movie databse api
    getGenres will return a list of all available genres
    
    getBadMoviesByGenre will get the worst rated movies for the genre
    specified by the genreId

--- server/helpers/apiHelpers.js
+++ server/helpers/apiHelpers.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const axios = require('axios');
-const { API_KEY } = require('../../config.js');
+const { API_KEY } = require('../../config/config.js');
 
 // write out logic/functions required to query TheMovieDB.org
 
@@ -10,3 +10,11 @@ const { API_KEY } = require('../../config.js');
 // Get your API Key and save it in your config file
 
 // Don't forget to export your functions and require them within your server file
+
+module.exports.getGenres = () => {
+  return axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`)
+};
+
+module.exports.getBadMoviesByGenre = (genreId) => {
+  return axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.asc&include_adult=false&include_video=false&page=1&with_genres=${genreId}`)
+};
